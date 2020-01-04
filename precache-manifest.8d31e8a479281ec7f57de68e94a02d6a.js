a387df79c170ac413e6f640496f1bafacfb45799
ejdweck <evanjordandweck@gmail.com>
2020-01-04 19:25:06 +0800

    Updates

---
+++ precache-manifest.8d31e8a479281ec7f57de68e94a02d6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b4ada12ed602aa0d8e7e17f1be49f5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e1455dddae86977627e0",
+    "url": "/portfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "2addf50251e4ba13117f",
+    "url": "/portfolio/static/js/2.88d7a294.chunk.js"
+  },
+  {
+    "revision": "ffd092476f661bed2744d749a4c8ce11",
+    "url": "/portfolio/static/js/2.88d7a294.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1455dddae86977627e0",
+    "url": "/portfolio/static/js/main.f5503af8.chunk.js"
+  },
+  {
+    "revision": "2ae66d7f5100d1860230",
+    "url": "/portfolio/static/js/runtime-main.a7df4a32.js"
+  },
+  {
+    "revision": "1f685bc01543fabdcfeb870d10252df8",
+    "url": "/portfolio/static/media/headshot.1f685bc0.jpeg"
+  },
+  {
+    "revision": "d57a4ef0bc44cb3c3bbff97529ce84bd",
+    "url": "/portfolio/static/media/melons.d57a4ef0.gif"
+  },
+  {
+    "revision": "1a4ba160006f971ac6b0480992b08ade",
+    "url": "/portfolio/static/media/name-underline.1a4ba160.svg"
+  },
+  {
+    "revision": "ed4988c89492f74c6d5ff45b1cc80ee5",
+    "url": "/portfolio/static/media/todo-concept-v1.ed4988c8.svg"
+  }
+]);
