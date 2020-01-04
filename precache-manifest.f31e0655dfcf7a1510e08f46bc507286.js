04a657160b477403e167707174bdc08b8314f75e
ejdweck <evanjordandweck@gmail.com>
2020-01-04 23:36:29 +0800

    Updates

---
+++ precache-manifest.f31e0655dfcf7a1510e08f46bc507286.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82c334d59a7b51e020d57f5a3406a91",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71d95cf3eee7c4179a25",
+    "url": "/portfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "fdd0b14c071aad653483",
+    "url": "/portfolio/static/js/2.0d042792.chunk.js"
+  },
+  {
+    "revision": "ffd092476f661bed2744d749a4c8ce11",
+    "url": "/portfolio/static/js/2.0d042792.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71d95cf3eee7c4179a25",
+    "url": "/portfolio/static/js/main.eba54cda.chunk.js"
+  },
+  {
+    "revision": "2ae66d7f5100d1860230",
+    "url": "/portfolio/static/js/runtime-main.a7df4a32.js"
+  },
+  {
+    "revision": "fd96caeb8ccef0b7e075845510f41796",
+    "url": "/portfolio/static/media/anvil.fd96caeb.svg"
+  },
+  {
+    "revision": "c0d8fd6774b8a5c5fcef84c2a42fd9b9",
+    "url": "/portfolio/static/media/bbox.c0d8fd67.gif"
+  },
+  {
+    "revision": "1f685bc01543fabdcfeb870d10252df8",
+    "url": "/portfolio/static/media/headshot.1f685bc0.jpeg"
+  },
+  {
+    "revision": "0f93cfb64a34e05c538f974c1a4de640",
+    "url": "/portfolio/static/media/melons.0f93cfb6.gif"
+  },
+  {
+    "revision": "1a4ba160006f971ac6b0480992b08ade",
+    "url": "/portfolio/static/media/name-underline.1a4ba160.svg"
+  },
+  {
+    "revision": "189517d901964628b2d2be64b6156b34",
+    "url": "/portfolio/static/media/realcourseguide.189517d9.gif"
+  },
+  {
+    "revision": "6ac3cf6d2664561258b59c680f57174c",
+    "url": "/portfolio/static/media/todo-concept-v1.6ac3cf6d.svg"
+  },
+  {
+    "revision": "a21d38b2d9afb3286e490a499357569a",
+    "url": "/portfolio/static/media/trailblazers-chatroom.a21d38b2.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eb2a799723d7b0478d40a895fda06438.js"
+  "/portfolio/precache-manifest.f31e0655dfcf7a1510e08f46bc507286.js"
 );
 
 self.addEventListener('message', (event) => {
